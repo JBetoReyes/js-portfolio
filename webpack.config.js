dc027f5b86ad722ff5e202670ef7f8de45708cc9
JBetoReyes <josereyes55@hotmail.com>
2021-03-31 14:37:59 -0600

    optimizing images

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -23,6 +24,9 @@ module.exports = {
       {
           test: /\.css|\.styl$/i,
           use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
+      }, {
+          test: /\.png/,
+          type: 'asset/resource'
       }
     ],
   },
@@ -32,6 +36,12 @@ module.exports = {
         template: './public/index.html',
         filename: './index.html'
       }),
-      new MiniCssExtractPlugin()
+      new MiniCssExtractPlugin(),
+      new CopyPlugin({
+          patterns: [{
+              from: path.resolve(__dirname, "src", "assets/images"),
+              to: "assets/images"
+          }]
+      }),
   ]
 };
