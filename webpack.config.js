e8690eab256d3309a1e2084cbc3e762edf6df763
JBetoReyes <josereyes55@hotmail.com>
2021-04-01 11:50:06 -0600

    adding fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
+    assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   resolve: {
     extensions: [".js"],
@@ -22,26 +23,43 @@ module.exports = {
         },
       },
       {
-          test: /\.css|\.styl$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
-      }, {
-          test: /\.png/,
-          type: 'asset/resource'
-      }
+        test: /\.css|\.styl$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
     ],
   },
   plugins: [
-      new HtmlWebpackPlugin({
-        inject: true,
-        template: './public/index.html',
-        filename: './index.html'
-      }),
-      new MiniCssExtractPlugin(),
-      new CopyPlugin({
-          patterns: [{
-              from: path.resolve(__dirname, "src", "assets/images"),
-              to: "assets/images"
-          }]
-      }),
-  ]
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images",
+        },
+      ],
+    }),
+  ],
 };
