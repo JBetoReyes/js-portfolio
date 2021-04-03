773fedf53d9a663f8f4ed2e02dc44f89604a1395
JBetoReyes <josereyes55@hotmail.com>
2021-04-02 18:48:12 -0600

    adding dev server

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -73,5 +73,11 @@ module.exports = {
       ],
     }),
     new Dotenv()
-  ]
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    port: 3006
+  }
 };
